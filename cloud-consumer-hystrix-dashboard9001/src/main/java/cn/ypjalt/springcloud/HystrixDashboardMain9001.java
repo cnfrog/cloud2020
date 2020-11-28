0d341c01d92b416453e2a5e34fe99b8815e22722
ProYuan <50544216@qq.com>
2020-11-28 23:06:19 +0800

    Hystrix DashBoard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/cn/ypjalt/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package cn.ypjalt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yuanpengjun
+ * DateTime: 2020/11/28 9:54 下午
+ * Description: No Description
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
