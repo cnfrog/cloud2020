42080bf298edfad8afa1567b86bb07fb9a053059
ProYuan <50544216@qq.com>
2020-12-05 21:32:58 +0800

    Spring Cloud Bus

---
+++ cloud-config-client-3366/src/main/java/cn/ypjalt/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package cn.ypjalt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yuanpengjun
+ * DateTime: 2020/12/5 6:50 下午
+ * Description: No Description
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/cn/ypjalt/springcloud/controller/ConfigController.java
@@ -0,0 +1,27 @@
+package cn.ypjalt.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yuanpengjun
+ * DateTime: 2020/12/2 11:11 下午
+ * Description: No Description
+ */
+@RestController
+@RefreshScope
+public class ConfigController {
+    @Value("${config}")
+    private String configInfo;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort: " + serverPort + "\t\nconfigInfo: " + configInfo;
+    }
+}
